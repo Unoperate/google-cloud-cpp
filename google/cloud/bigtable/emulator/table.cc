cad64505281a1a295ae6ae6c1773684f0dea4325
Brian Gitonga Marete <bgmarete@gmail.com>
2025-06-12 19:45:16 +0300

    emulator: mechanical formatting fixes by ci tool.

--- google/cloud/bigtable/emulator/table.cc
+++ google/cloud/bigtable/emulator/table.cc
@@ -556,7 +556,6 @@ Table::ReadModifyWriteRow(
   return std::move(maybe_response.value());
 }
 
-
 // NOLINTBEGIN(readability-convert-member-functions-to-static)
 Status RowTransaction::AddToCell(
     ::google::bigtable::v2::Mutation_AddToCell const& add_to_cell) {
@@ -726,7 +725,8 @@ void ProcessReadModifyWriteResult(
   // Record the cell in our local mini table here to use in
   // assembling a row of changed cells for return.
   tmp_families[rule.family_name()].SetCell(row_key, rule.column_qualifier(),
-                                           result.timestamp, std::move(result.value));
+                                           result.timestamp,
+                                           std::move(result.value));
 }
 
 // NOLINTBEGIN(readability-function-cognitive-complexity)
